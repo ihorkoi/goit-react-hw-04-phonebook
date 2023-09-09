26b82e384420b05780cc764bedc3dbda886a9fde
ihorkoi <koleev.igor@gmail.com>
2023-09-09 10:07:01 +0300

    rewrite on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 
 const LS_KEY = 'saved_contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  addContact = contact => {
-    const alreadyIn = this.state.contacts.find(
+  const addContact = contact => {
+    const alreadyIn = contacts.find(
       existingContact =>
         existingContact.name.toLowerCase() === contact.name.toLowerCase()
     );
@@ -20,66 +18,56 @@ export class App extends Component {
       alert(`${contact.name} is already in contacts`);
       return;
     }
-
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, contact],
-      };
-    });
+    setContacts([...contacts, contact]);
   };
-  removeContact = id => {
-    this.setState(state => ({
-      contacts: state.contacts.filter(contact => contact.id !== id),
-    }));
+  const removeContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
-  handleFilterState = evt => {
-    this.setState({ filter: evt.target.value });
+  const handleFilterState = evt => {
+    setFilter(evt.target.value);
   };
-  filteredContacts = ({ contacts, filter }) => {
+  const filteredContacts = ( contacts, filter ) => {
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase())
     );
   };
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem(LS_KEY, JSON.stringify([...this.state.contacts]));
-    }
-  }
-  componentDidMount() {
+  useEffect(() => {
     if (localStorage.getItem(LS_KEY)) {
-      this.setState({ contacts: JSON.parse(localStorage.getItem(LS_KEY)) });
+      setContacts(JSON.parse(localStorage.getItem(LS_KEY)));
     }
-  }
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact}></ContactForm>
+  }, []);
+  useEffect(()=>{
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts))
+  },[contacts])
 
-        {this.state.contacts.length === 0 ? (
-          <p>You don`t have any contact yet</p>
-        ) : (
-          <>
-            <h2>Contacts</h2>
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact}></ContactForm>
 
-            <Filter onChange={this.handleFilterState}></Filter>
-            <ContactsList
-              props={this.filteredContacts(this.state)}
-              removeContact={this.removeContact}
-            />
-          </>
-        )}
-      </div>
-    );
-  }
-}
+      {contacts.length === 0 ? (
+        <p>You don`t have any contact yet</p>
+      ) : (
+        <>
+          <h2>Contacts</h2>
+
+          <Filter onChange={handleFilterState}></Filter>
+          <ContactsList
+            props={filteredContacts(contacts, filter)}
+            removeContact={removeContact}
+          />
+        </>
+      )}
+    </div>
+  );
+};
